f5463813a89920dd96e4ad4859b1eb840bf46e3e
Joy Evans <joyevans@me.com>
2020-09-22 00:27:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,23 @@ require 'pry'
 def using_concat(my_favorite_things, more_favs)
   
 end
+
+def using_insert
+  
+end
+
+def using_uniq
+  
+end
+
+def using_flatten
+  
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
