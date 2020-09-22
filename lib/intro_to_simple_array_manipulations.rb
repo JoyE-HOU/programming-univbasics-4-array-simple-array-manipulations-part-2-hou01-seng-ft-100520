bcaf773f4cea3f0560cadc316145b2d694edcd27
Joy Evans <joyevans@me.com>
2020-09-22 00:47:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,7 +3,7 @@ require 'pry'
 def using_concat(my_favorite_things, more_favs)
   all_my_favs = []
   
-  all_my_favs = my_favorite_things.concat([more_favs])
+  all_my_favs = my_favorite_things.concat(more_favs)
   
 end
 
